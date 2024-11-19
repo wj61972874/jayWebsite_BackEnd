da155fbbd5217a8dd8bed1660ee862ef389c0ddc
Wangjie <m15205519363@163.com>
2024-11-19 20:59:45 +0800

    first commit

--- src/main/java/com/jayblog/weisite/WeisiteApplication.java
+++ src/main/java/com/jayblog/weisite/WeisiteApplication.java
@@ -1,23 +1,31 @@
 package com.jayblog.weisite;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-public class WeisiteApplication {
+@Import(com.jayblog.weisite.config.DotenvConfig.class)
+public class WeisiteApplication implements CommandLineRunner {
+
+    private final Dotenv dotenv;
+
+    public WeisiteApplication(Dotenv dotenv) {
+        this.dotenv = dotenv;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(WeisiteApplication.class, args);
     }
 
-}
-
-@RestController
-class HelloController {
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello, World!";
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("MYSQL_HOST======: " + dotenv.get("MYSQL_HOST"));
+        System.out.println("MYSQL_PORT======: " + dotenv.get("MYSQL_PORT"));
+        System.out.println("MYSQL_DB======: " + dotenv.get("MYSQL_DB"));
+        System.out.println("MYSQL_USER======: " + dotenv.get("MYSQL_USER"));
+        System.out.println("MYSQL_PASSWORD======: " + dotenv.get("MYSQL_PASSWORD"));
     }
 }
