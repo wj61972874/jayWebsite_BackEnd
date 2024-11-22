e7f6ab42315d0f3d46f0e66bb48335eceacce3a8
Wangjie <m15205519363@163.com>
2024-11-23 01:24:06 +0800

    update for manage blog

--- src/main/java/com/jayblog/weisite/config/JwtAuthenticationFilter.java
+++ src/main/java/com/jayblog/weisite/config/JwtAuthenticationFilter.java
@@ -1,11 +1,14 @@
 package com.jayblog.weisite.config;
 
+import com.jayblog.weisite.service.impl.ArticleServiceImpl;
 import com.jayblog.weisite.utils.JwtUtil;
 import io.jsonwebtoken.Claims;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -19,6 +22,8 @@ import java.io.IOException;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
+
 
     @Autowired
     private JwtUtil jwtUtil;
@@ -36,19 +41,26 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
         if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
             jwt = authorizationHeader.substring(7);
+            logger.debug("jwt ==111 : {}", jwt);
             username = jwtUtil.extractUsername(jwt);
+            logger.debug("jwt ==222: {}", username);
         }
 
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            logger.debug("jwt ==333: {}", username);
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
-
+            logger.debug("jwt == 444: {}", userDetails);
             if (jwtUtil.validateToken(jwt)) {
+                logger.debug("jwt == 555: {}", userDetails);
                 UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
                         userDetails, null, userDetails.getAuthorities());
+                logger.debug("jwt == 666:");
                 authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                logger.debug("jwt == 777:");
                 SecurityContextHolder.getContext().setAuthentication(authentication);
             }
         }
+        logger.debug("jwt == 888:");
         chain.doFilter(request, response);
     }
 }
